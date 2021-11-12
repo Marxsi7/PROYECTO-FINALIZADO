9dcae8f9999814a8e08d159156c5c000b934f4bf
Marxsi7 <89942553+Marxsi7@users.noreply.github.com>
2021-11-12 17:33:24 -0300

    12,11 FINALIZADO

---
+++ login.js
@@ -0,0 +1,14 @@
+function ir(){
+
+var c=1234;
+var u="bruno";
+
+if(document.form.password.value==c && document.form.login.value==u){
+alert ("Bienvenido Bruno");
+window.location="principalopcion2.html";}
+
+else{
+alert("porfavor ingrese, nombre y usuario correcto.");
+}
+
+}
